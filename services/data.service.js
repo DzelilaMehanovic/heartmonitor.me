63a309adfb2a5781d0cdb21c1e128f73ed690e95
DzelilaMehanovic <dzelila.mehanovic@stu.ibu.edu.ba>
2018-01-16 20:49:30 +0100

    message

---
+++ services/data.service.js
@@ -0,0 +1,48 @@
+var config = require('config.json');
+var _ = require('lodash');
+var jwt = require('jsonwebtoken');
+var bcrypt = require('bcryptjs');
+var Q = require('q');
+var mongo = require('mongoskin');
+var db = mongo.db(config.connectionString, { native_parser: true });
+db.bind('diets');
+db.bind('diseases');
+
+
+var service = {};
+service.getDiets = getDiets;
+service.getDiseases = getDiseases;
+
+module.exports = service;
+
+function getDiets() {
+    var deferred = Q.defer();
+
+    db.diets.findItems({}, function (err, diets) {
+        if (err) deferred.reject(err.name + ': ' + err.message);
+
+        if (diets) {
+            deferred.resolve(_.omit(diets, '_id'));
+        } else {
+            deferred.resolve();
+        }
+    });
+
+    return deferred.promise;
+}
+
+function getDiseases() {
+    var deferred = Q.defer();
+
+    db.diseases.findItems({}, function (err, diseases) {
+        if (err) deferred.reject(err.name + ': ' + err.message);
+
+        if (diseases) {
+            deferred.resolve(_.omit(diseases, '_id'));
+        } else {
+            deferred.resolve();
+        }
+    });
+
+    return deferred.promise;
+}
---
+++ services/user.service.js
@@ -0,0 +1,173 @@
+﻿var config = require('config.json');
+var _ = require('lodash');
+var jwt = require('jsonwebtoken');
+var bcrypt = require('bcryptjs');
+var Q = require('q');
+var mongo = require('mongoskin');
+var db = mongo.db(config.connectionString, { native_parser: true });
+db.bind('users');
+db.bind('diets');
+db.bind('pressure');
+
+var service = {};
+
+service.authenticate = authenticate;
+service.getById = getById;
+service.create = create;
+service.update = update;
+service.delete = _delete;
+service.savePressure = savePressure;
+service.getPressure = getPressure;
+
+module.exports = service;
+function authenticate(username, password) {
+    var deferred = Q.defer();
+
+    db.users.findOne({ username: username }, function (err, user) {
+        if (err) deferred.reject(err.name + ': ' + err.message);
+
+        if (user && bcrypt.compareSync(password, user.hash)) {
+            deferred.resolve(jwt.sign({ sub: user._id }, config.secret));
+        } else {
+            deferred.resolve();
+        }
+    });
+
+    return deferred.promise;
+}
+
+function getById(_id) {
+    var deferred = Q.defer();
+
+    db.users.findById(_id, function (err, user) {
+        if (err) deferred.reject(err.name + ': ' + err.message);
+
+        if (user) {
+            deferred.resolve(_.omit(user, 'hash'));
+        } else {
+            deferred.resolve();
+        }
+    });
+
+    return deferred.promise;
+}
+
+function getPressure(user_id) {
+    var deferred = Q.defer();
+
+    db.pressure.findItems({user_id : user_id}, function (err, pressures) {
+        if (err) deferred.reject(err.name + ': ' + err.message);
+
+        if (pressures) {
+            deferred.resolve(_.omit(pressures, '_id'));
+        } else {
+            deferred.resolve();
+        }
+    });
+
+    return deferred.promise;
+}
+function create(userParam) {
+    var deferred = Q.defer();
+
+    // validation
+    db.users.findOne(
+        { username: userParam.username },
+        function (err, user) {
+            if (err) deferred.reject(err.name + ': ' + err.message);
+
+            if (user) {
+                deferred.reject('Username "' + userParam.username + '" is already taken');
+            } else {
+                createUser();
+            }
+        });
+
+    function createUser() {
+        var user = _.omit(userParam, 'password');
+        user.hash = bcrypt.hashSync(userParam.password, 10);
+
+        db.users.insert(
+            user,
+            function (err, doc) {
+                if (err) deferred.reject(err.name + ': ' + err.message);
+
+                deferred.resolve();
+            });
+    }
+
+    return deferred.promise;
+}
+
+function update(_id, userParam) {
+    var deferred = Q.defer();
+    db.users.findById(_id, function (err, user) {
+        if (err) deferred.reject(err.name + ': ' + err.message);
+
+        if (user.username !== userParam.username) {
+            db.users.findOne(
+                { username: userParam.username },
+                function (err, user) {
+                    if (err) deferred.reject(err.name + ': ' + err.message);
+
+                    if (user) {
+                        deferred.reject('Username "' + req.body.username + '" already exists')
+                    } else {
+                        updateUser();
+                    }
+                });
+        } else {
+            updateUser();
+        }
+    });
+
+    function updateUser() {
+        var set = {
+            firstName: userParam.firstName,
+            lastName: userParam.lastName,
+            username: userParam.username,
+        };
+        if (userParam.password) {
+            set.hash = bcrypt.hashSync(userParam.password, 10);
+        }
+
+        db.users.update(
+            { _id: mongo.helper.toObjectID(_id) },
+            { $set: set },
+            function (err, doc) {
+                if (err) deferred.reject(err.name + ': ' + err.message);
+
+                deferred.resolve();
+            });
+    }
+
+    return deferred.promise;
+}
+
+function _delete(_id) {
+    var deferred = Q.defer();
+
+    db.users.remove(
+        { _id: mongo.helper.toObjectID(_id) },
+        function (err) {
+            if (err) deferred.reject(err.name + ': ' + err.message);
+
+            deferred.resolve();
+        });
+
+    return deferred.promise;
+}
+
+
+function savePressure(userParam){
+      var deferred = Q.defer();
+          var pressure = userParam;
+          db.pressure.insert(
+              pressure,
+              function (err, doc) {
+                  if (err) deferred.reject(err.name + ': ' + err.message);
+
+                  deferred.resolve();
+              });
+      return deferred.promise;
+}
